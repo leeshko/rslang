46a3f688c386d0fa0d81dd4c42168c59c140183c
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-26 08:50:22 +0500

    fix: fix name issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import { useLocation, Route, Switch, Redirect } from 'react-router-dom';
+import React from "react";
+import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 
-import './App.css';
-import AboutTeam from './components/aboutTeam/aboutTeam';
-import Games from './components/games/games';
-import Header from './components/header/header';
-import LoginRegister from './components/loginRegister/loginRegistration';
-import Main from './components/main/main';
-import NotFound from './components/notFound/notFound'
+import "./App.css";
+import AboutTeam from "./components/AboutTeam/AboutTeam";
+import Games from "./components/games/games";
+import Header from "./components/header/header";
+import LoginRegister from "./components/loginRegister/loginRegistration";
+import Main from "./components/main/main";
+import NotFound from "./components/notFound/notFound";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route path='/404' component={NotFound} />
-        <Route path='/registration' component={LoginRegister}/>
+        <Route path="/404" component={NotFound} />
+        <Route path="/registration" component={LoginRegister} />
         <Route>
           <Header />
           <Switch>
-            <Route path='/' exact component={Main}/>
-            <Route path='/AboutTeam' component={AboutTeam}/>
-            <Route path='/games' component={Games}/>
-            <Route render={()=>(
-                <Redirect to="/404"/>
-            )} />
+            <Route path="/" exact component={Main} />
+            <Route path="/AboutTeam" component={AboutTeam} />
+            <Route path="/games" component={Games} />
+            <Route render={() => <Redirect to="/404" />} />
           </Switch>
         </Route>
       </Switch>
-
     </>
   );
 }
