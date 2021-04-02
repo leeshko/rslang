62147dea261c434b790a07de84bec4f1b00a84e4
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-04-02 08:59:11 +0500

    feat: add app context

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { useLocation, Route, Switch, Redirect } from "react-router-dom";
+import {
+  useLocation,
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 import "./App.css";
 import AboutTeam from "./components/AboutTeam/AboutTeam";
@@ -8,24 +14,28 @@ import Header from "./components/header/header";
 import LoginRegister from "./components/loginRegister/loginRegistration";
 import Main from "./components/main/main";
 import NotFound from "./components/notFound/notFound";
-
+import RSLangContext from "./components/context/context";
+import { defaultState, reducer } from "./components/reducer/reducer";
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, defaultState);
   return (
-    <>
-      <Switch>
-        <Route path="/404" component={NotFound} />
-        <Route path="/registration" component={LoginRegister} />
-        <Route>
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Main} />
-            <Route path="/AboutTeam" component={AboutTeam} />
-            <Route path="/games" component={Games} />
-            <Route render={() => <Redirect to="/404" />} />
-          </Switch>
-        </Route>
-      </Switch>
-    </>
+    <RSLangContext.Provider value={{ ...state, dispatch }}>
+      <Router>
+        <Switch>
+          <Route path="/404" component={NotFound} />
+          <Route path="/registration" component={LoginRegister} />
+          <Route>
+            <Header />
+            <Switch>
+              <Route path="/" exact component={Main} />
+              <Route path="/AboutTeam" component={AboutTeam} />
+              <Route path="/games" component={Games} />
+              <Route render={() => <Redirect to="/404" />} />
+            </Switch>
+          </Route>
+        </Switch>
+      </Router>
+    </RSLangContext.Provider>
   );
 }
 
